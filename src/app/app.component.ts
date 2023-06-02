755a7ccf4ae65366a6e7a2bcab63361d78c0ab27
Tirth <tpatel@argusoft.in>
2023-06-02 10:00:46 +0530

    ui design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tail-project';
+  isCollapsed: boolean;
+  isChangeProfileSelected: boolean;
+  menu: Array<any> = [
+    {
+      sectionName: 'Investor',
+      menuItems: [
+        {
+          iconName: 'investor-dashboard',
+          title: 'Investor',
+          link: 'dashboard/investor'
+        },
+        {
+          iconName: 'profile-settings',
+          title: 'KYC Profile Settings',
+          link: ''
+        },
+        {
+          iconName: 'funds',
+          title: 'My Funds',
+          link: ''
+        },
+        {
+          iconName: 'bell',
+          title: "Notifications",
+          link: ''
+        }
+      ],
+    },
+    {
+      sectionName: 'Partner',
+      menuItems: [
+        {
+          iconName: 'bar-chart',
+          title: 'All Funds',
+          link:''
+        },
+        {
+          iconName: 'subscription-dashboard',
+          title: 'Subscription Dashboard',
+          link:''
+        },
+        {
+          iconName: 'kyc-dashboard',
+          title: 'KYC Dashboard',
+          link:''
+        },
+        {
+          iconName: 'tax-dashboard',
+          title: 'Tax Dashboard',
+          link: ''
+        },
+      ],
+    }
+  ];
+  constructor() {
+    this.isCollapsed = false;
+    this.isChangeProfileSelected = false;
+  }
+
+  public toggleCollapsed() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+  public setIsSelected(value: boolean){
+    this.isChangeProfileSelected = value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +27,53 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatTabsModule,
+    FormsModule,
+    MatDividerModule,
+    MatChipsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatTooltipModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(iconsRegistery: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconsRegistery.addSvgIcon('search', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/search.svg'));
+    iconsRegistery.addSvgIcon('bell', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/bell.svg'));
+    iconsRegistery.addSvgIcon('printer', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/printer.svg'));
+    iconsRegistery.addSvgIcon('chevron-down', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/chevron-down.svg'));
+    iconsRegistery.addSvgIcon('chevron-left', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/chevron-left.svg'));
+    iconsRegistery.addSvgIcon('profile', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/profile.svg'));
+    iconsRegistery.addSvgIcon('investor-dashboard', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/investor-dashboard.svg'));
+    iconsRegistery.addSvgIcon('kyc-dashboard', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/kyc-dashboard.svg'));
+    iconsRegistery.addSvgIcon('right-arrow', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/right-arrow.svg'));
+    iconsRegistery.addSvgIcon('subscription-dashboard', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/subscription-dashboard.svg'));
+    iconsRegistery.addSvgIcon('tax-dashboard', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/tax-dashboard.svg'));
+    iconsRegistery.addSvgIcon('profile-settings', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/profile-settings.svg'));
+    iconsRegistery.addSvgIcon('funds', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/funds.svg'));
+    iconsRegistery.addSvgIcon('bar-chart', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/bar-chart.svg'));
+    iconsRegistery.addSvgIcon('collapse-arrows', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/collapse-arrows.svg'));
+    iconsRegistery.addSvgIcon('expand-arrows', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/expand-arrows.svg'));
+    iconsRegistery.addSvgIcon('attachment', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/attachment.svg'));
+    iconsRegistery.addSvgIcon('close', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/close.svg'));
+    iconsRegistery.addSvgIcon('delete', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/delete.svg'));
+    iconsRegistery.addSvgIcon('download', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/download.svg'));
+    iconsRegistery.addSvgIcon('settings-outline', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/settings-outline.svg'));
+    iconsRegistery.addSvgIcon('settings-solid', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/settings-solid.svg'));
+    iconsRegistery.addSvgIcon('hide-password', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/hide-password.svg'));
+    iconsRegistery.addSvgIcon('show-password', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/show-password.svg'));
+    iconsRegistery.addSvgIcon('arrow-left', sanitizer.bypassSecurityTrustResourceUrl('/assets/img/arrow-left.svg'));
+  }
+}
